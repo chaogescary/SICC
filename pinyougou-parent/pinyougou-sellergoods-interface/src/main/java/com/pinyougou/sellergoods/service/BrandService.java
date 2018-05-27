c987ce5dd3b42147344976b925eeb1ffc4e6c389
SICC <chaogescary@126.com>
2018-05-27 16:39:23 +0800

    第2章 品牌管理
    一级学习目标
    	运用AngularJS前端框架的常用指令
    	实现品牌列表的前端代码
    	实现品牌列表分页的前端代码
    	实现增加品牌的功能
    	实现修改品牌的功能
    	实现删除品牌的功能
    	实现品牌条件查询的功能
    
    二级学习目标
    	运用表达式
    	运用双向绑定
    	运用ng-init指令
    	运用ng-controller指令
    	运用ng-click指令
    	运用ng-repeat指令
    	运用内置服务$http发送get请求
    	能够运用angualrJS的分页组件实现列表分页功能
    	运用内置服务$http发送post请求

--- pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -4,7 +4,21 @@ import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
 
+import entity.PageResults;
+
 public interface BrandService {
 
 	public List<TbBrand> findAll();
+	
+	public PageResults findPage(int pageNum,int pageSize);
+	
+	public void add(TbBrand brand);
+	
+	public void update(TbBrand brand);
+	
+	public TbBrand findOne(Long id);
+	
+	public void delete(Long [] ids);
+	
+	public PageResults findPage(TbBrand brand, int pageNum,int pageSize);
 }
--- pinyougou-parent/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou-parent/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.pinyougou.mapper.TbBrandMapper;
 import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.pojo.TbBrandExample;
+import com.pinyougou.pojo.TbBrandExample.Criteria;
 import com.pinyougou.sellergoods.service.BrandService;
+
+import entity.PageResults;
 
 @Service
 public class BrandServiceImpl implements BrandService{
@@ -17,9 +23,55 @@ public class BrandServiceImpl implements BrandService{
 	
 	@Override
 	public List<TbBrand> findAll() {
-		// TODO Auto-generated method stub
 		return brandMapper.selectByExample(null);
 	}
 
+	@Override
+	public PageResults findPage(int pageSize, int pageNum) {
+		PageHelper.startPage(pageNum, pageSize);
+		Page<TbBrand> page = (Page<TbBrand>) brandMapper.selectByExample(null);
+		return new PageResults(page.getTotal(),page.getResult());
+	}
+
+	@Override
+	public void add(TbBrand brand) {
+		brandMapper.insert(brand);
+	}
+
+	@Override
+	public void update(TbBrand brand) {
+		brandMapper.updateByPrimaryKey(brand);
+	}
+
+	@Override
+	public TbBrand findOne(Long id) {
+		return brandMapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public void delete(Long[] ids) {
+		for(Long id:ids){
+			brandMapper.deleteByPrimaryKey(id);
+		}	
+	}
+
+	@Override
+	public PageResults findPage(TbBrand brand, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);		
+		TbBrandExample example=new TbBrandExample();
+		Criteria criteria = example.createCriteria();		
+		if(brand!=null){
+			if(brand.getName()!=null && brand.getName().length()>0){
+				criteria.andNameLike("%"+brand.getName()+"%");
+			}
+			if(brand.getFirstChar()!=null && brand.getFirstChar().length()>0){
+				criteria.andFirstCharEqualTo(brand.getFirstChar());
+			}		
+		}		
+		Page<TbBrand> page= (Page<TbBrand>)brandMapper.selectByExample(example);	
+		return new PageResults(page.getTotal(), page.getResult());
+	}
+
+	
 	
 }
